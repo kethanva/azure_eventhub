5060260c5320a8f739170377a7c85ed4e92b30eb
Kethan VA <kethan@netreo.com>
2022-07-27 18:16:28 +0530

    First one

---
+++ src/main/java/test/App2.java
@@ -0,0 +1,52 @@
+package test;
+import com.azure.messaging.eventhubs.*;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2
+{
+     static final String connectionString = "";
+     static final String eventHubName = "eventhub1";
+
+    public static void main(String[] args) {
+        publishEvents();
+    }
+
+    public static void publishEvents() {
+        // create a producer client
+        EventHubProducerClient producer = new EventHubClientBuilder()
+                .connectionString(connectionString, eventHubName)
+                .buildProducerClient();
+
+        // sample events in an array
+        List<EventData> allEvents = Arrays.asList(new EventData("Foo"), new EventData("Bar"));
+
+        // create a batch
+        EventDataBatch eventDataBatch = producer.createBatch();
+
+        for (EventData eventData : allEvents) {
+            // try to add the event from the array to the batch
+            if (!eventDataBatch.tryAdd(eventData)) {
+                // if the batch is full, send it and then create a new batch
+                producer.send(eventDataBatch);
+                eventDataBatch = producer.createBatch();
+
+                // Try to add that event that couldn't fit before.
+                if (!eventDataBatch.tryAdd(eventData)) {
+                    throw new IllegalArgumentException("Event is too large for an empty batch. Max size: "
+                            + eventDataBatch.getMaxSizeInBytes());
+                }
+            }
+        }
+        // send the last batch of remaining events
+        if (eventDataBatch.getCount() > 0) {
+            producer.send(eventDataBatch);
+        }
+        producer.close();
+    }
+
+}
